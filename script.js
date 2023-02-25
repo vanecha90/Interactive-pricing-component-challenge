6b8fc5b77af503372731b0f303b526f0a82c7402
Vanessa Collado <vanessa.collado90@gmail.com>
2023-02-25 11:37:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,45 @@
 //add variants with query selectors of all items that changes when slider move
 let view = document.getElementById("pageView");
 let cost = document.getElementById("cost");
-let cost2 = document.getElementById("cost2");
-let slider = document.getElementById("price-slider");
+let cost2 = document.getElementById("cost2");//this is for mobile
+let sliderR = document.getElementById("price-slider");
 let toggle = document.getElementById("billing");
-let range = document.getElementsByClassName("range");
 //make array with pageviews values
 let pageViews = ["10K", "50K", "100K", "500K", "1M"];
 //make array with montly price
 let perMonth = [8, 12, 16, 24, 36];
+//toggle is off
+let yearlyOn = false;
 
-//make slider move and change number
-slider.addEventListener("input", function () {
-  cost.innerHTML = perMonth[4];
+//make slider move and change number and views
+sliderR.addEventListener("input", function () {
+  updateVal();
+  cost.innerHTML = perMonth[sliderR.value];
+  view.innerHTML = pageViews[sliderR.value];
+  let colorbar = sliderR.value * 25;
+  sliderR.style.background = `linear-gradient(
+    to right,
+    hsl(174, 77%, 80%) 0%,
+    hsl(174, 77%, 80%) ${colorbar}%,
+    hsl(224, 65%, 95%) 0%,
+    hsl(224, 65%, 95%) 100%`
 });
+
+//toogle true/false... this will tell if the toggle is on
+toggle.addEventListener('change', function(){
+  if(yearlyOn === false){
+    yearlyOn = true;
+  }else{
+    yearlyOn = false;
+  }
+  updateVal();
+});
+
+//if tooggle is on it will execute this..chaning the value
+function updateVal(){
+  if(yearlyOn){
+    cost.innerHTML = perMonth[sliderR.value] * .75;
+  }else{
+    cost.innHTML =perMonth[sliderR.value];
+  }
+}
